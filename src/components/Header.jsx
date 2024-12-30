43165ff51a1273c0c46e3fddb01c46f4194c8338
Eren Sefa Öztürk <69047271+MakroEs@users.noreply.github.com>
2024-12-30 20:31:03 +0300

    Add header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import MenuItem from "./MenuItem";
+import { FaHome } from "react-icons/fa";
+import { BsInfoCircleFill } from "react-icons/bs";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+      <div className="flex gap-4">
+        <MenuItem title="home" address="/" Icon={FaHome} />
+        <MenuItem title="about" address="/about" Icon={BsInfoCircleFill} />
+      </div>
+      <Link href={"/"} className=" flex gap-1 items-center ">
+        <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
+          IMDB
+        </span>
+        <span className="text-xl hidden sm:inline"> Clone</span>
+      </Link>
+    </div>
+  );
+}
