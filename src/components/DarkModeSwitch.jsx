6daeaa88611787519ea0281a58e8df2fd00a0051
Eren Sefa Öztürk <69047271+MakroEs@users.noreply.github.com>
2025-01-02 00:49:39 +0300

    error

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { MdLightMode, MdDarkMode } from "react-icons/md";
+
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
@@ -15,12 +16,12 @@ export default function DarkModeSwitch() {
         (currentTheme === "dark" ? (
           <MdLightMode
             onClick={() => setTheme("light")}
-            className="text-xl cursor-pointer "
+            className="text-xl cursor-pointer hover:text-amber-500"
           />
         ) : (
           <MdDarkMode
             onClick={() => setTheme("dark")}
-            className="text-xl cursor-pointer "
+            className="text-xl cursor-pointer hover:text-amber-500"
           />
         ))}
     </div>
